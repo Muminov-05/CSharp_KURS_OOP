3562435fe5d90e6456a6b4119b5b73323db42967
Amirjon <amirjon.muminov2005@gmail.com>
2026-01-15 21:52:15 +0500

    ClassWork

---
+++ 15_01_2026_lecture6/ClassWork/Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//  Создайте класс под названием `Person` со следующими свойствами: `FirstName`, `LastName` и `Age`.
+//  Вам следует создать метод под названием `GetInfo()`. Метод должен возвращать информацию о человеке.
+
+Person person = new Person();
+Console.Write("Enter your FirstName : ");
+person.FirstName = Console.ReadLine();
+Console.Write("Enter your LastName :  ");
+person.LastName = Console.ReadLine();
+Console.Write("Enter your Age : ");
+person.Age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(" Output : ");
+person.GetInfo();
+
+class Person
+{
+    public string FirstName;
+    public string LastName;
+    public int Age;
+    public void GetInfo()
+    {
+        Console.WriteLine($" Firstname : {FirstName} ");
+        Console.WriteLine($" LastName : {LastName} ");
+        Console.WriteLine($" Age : {Age} ");
+    }
+}
+
+
---
+++ 15_01_2026_lecture6/ClassWork/Task_2/Program.cs
@@ -0,0 +1,75 @@
+﻿
+//Создайте класс под названием «Post» со следующими свойствами: `Text`, `Likes`, `Comments`.
+//**Атрибутес:**
+//***Text * *: текст сообщения.
+//***Tikes * *: количество лайков публикации.
+//* **Comments**: список комментариев к публикации.
+
+//***SetPost(text) * *: Создает новое сообщение .
+
+//* **Like()**: увеличивает количество лайков на один.
+
+//* **Comment(comment)**: добавляет новый комментарий к сообщению.
+
+//* **Info()**: отображает информацию о публикации, включая ее текст, количество лайков и комментариев.
+
+Post post = new Post();
+post.SetPost();
+post.AddLike();
+post.AddLike();
+post.AddComment();
+post.AddComment();
+post.GetInfo();
+
+
+class Post
+{
+    public string Text;
+    public int Likes;
+    public string[] Comment;
+
+    public Post()
+    {
+        Likes = 0;
+        Comment = new string[0];
+    }
+
+    public void SetPost()
+    {
+        Console.Write("Введите сообщение: ");
+        string str = Console.ReadLine();
+        Text = str;
+    }
+
+    public void AddLike()
+    {
+        Likes++;
+    }
+
+    public void AddComment()
+    {
+        Console.Write("Введите комментарий: ");
+        string com = Console.ReadLine();
+        int dlina = Comment.Length + 1;
+        string[] newComments = new string[dlina];
+        for (int i = 0; i < Comment.Length; i++)
+        {
+            newComments[i] = Comment[i];
+        }
+        newComments[dlina-1] = com;
+        Comment = newComments;
+    }
+
+    public void GetInfo()
+    {
+        Console.WriteLine("\n---Post------");
+        Console.WriteLine($"Text  : {Text}");
+        Console.WriteLine($"Likes : {Likes}");
+        Console.WriteLine("Comments:");
+
+        for (int i = 0; i < Comment.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {Comment[i]}");
+        }
+    }
+}
