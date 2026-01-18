8de57e2ed793c92a353d3fbab4c9837f98045746
Amirjon <amirjon.muminov2005@gmail.com>
2026-01-18 23:03:10 +0500

    ClassWork

---
+++ 17_01_2026_lecture7/ClassWork/Task_1/Program.cs
@@ -0,0 +1,43 @@
+﻿
+Teacher teacher = new Teacher(" Владимер", "OOП",5);
+Console.WriteLine(teacher.Teach());
+
+Console.Write(" Cтаж учителя : ");
+int staj = Convert.ToInt32(Console.ReadLine());
+teacher.SetExperience(staj);
+Console.WriteLine( $" Стаж Владимера {teacher.GetExperience()} лет.");
+
+class Teacher
+{
+    private string name;
+    private string subject;
+    private int experience;
+
+    public Teacher() {}
+    public Teacher( string new_name , string new_subject)
+    {
+        name = new_name;
+        subject = new_subject;
+        experience = 0;
+    }
+    public Teacher(string new_name, string new_subject, int new_experience)
+    {
+        name = new_name;
+        subject = new_subject;
+        experience = new_experience;
+    }
+
+    public string Teach()
+    {
+        return $"{name} преподает { subject}";
+    }
+
+    public void SetExperience(int years)
+    {
+        experience = years;
+    }
+    public int GetExperience() 
+    { 
+        return experience;
+    }
+}
---
+++ 17_01_2026_lecture7/ClassWork/Task_2/Program.cs
@@ -0,0 +1,57 @@
+﻿
+Student student = new Student();
+Console.WriteLine("Введите имя студента ");
+student.Name = Console.ReadLine();
+student.Study();
+Console.Write(" Введите новую оценку : ");
+int mark = Convert.ToInt32(Console.ReadLine());
+student.SetGrade(mark);
+Console.WriteLine($" Оценка у {student.Name} {student.GetGrade()}");
+
+class Student
+{
+    private string name ;
+    private int age ;
+    private int grade ;
+
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+    public int Grade
+    {
+        get { return grade; }
+        set { grade = value; }
+    }
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    public Student() {}
+    public Student(string name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+    public Student(string name, int age, int grade)
+    {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+    public void Study()
+    {
+        Console.WriteLine($" {name} учится ");
+    }
+    public void SetGrade(int newGrade)
+    {
+        grade = newGrade;
+    }
+    public int GetGrade()
+    {
+        return grade;
+    }
+}
