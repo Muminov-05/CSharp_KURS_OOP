31a07fe2f52b11caadf62a41c770b7ddeb2567f4
Amirjon <amirjon.muminov2005@gmail.com>
2026-01-22 12:18:24 +0500

    HomeWork

---
+++ 21_01_2026_lecture11/HomeWork/Task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿
+//Создайте список целых чисел и отобразите сумму всех элементов в списке.                
+
+List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+int sum = 0;
+foreach (int number in numbers)
+{
+    sum += number;
+}
+Console.WriteLine("The sum of all the elements in the list is: " + sum);
---
+++ 21_01_2026_lecture11/HomeWork/Task_10/Program.cs
@@ -0,0 +1,15 @@
+﻿
+// Создайте список и заполните его случайными числами. Распечатайте все числа. 
+
+List<int> numbers = new List<int>();
+Random rnd = new Random();
+
+for (int i = 0; i < 15; i++)
+{
+    numbers.Add(rnd.Next(1, 101)); // числа от 1 до 100
+}
+Console.WriteLine("Spisok radomnukh chisel :");
+foreach (int n in numbers)
+{
+    Console.Write(n + "  ");
+}
