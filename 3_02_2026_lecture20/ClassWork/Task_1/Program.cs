c2ded44abeda5612dac8410c2d6dcf957de96901
Amirjon <amirjon.muminov2005@gmail.com>
2026-02-04 02:01:45 +0500

    ClassWork

---
+++ 3_02_2026_lecture20/ClassWork/Task_1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Напишите программу, которая запрашивает у пользователя два числа и выводит результат их деления.                                                                        
+//Однако, если пользователь вводит некорректные данные (например, буквы вместо чисел), программа должна                                              
+//обрабатывать это и выводить сообщение об ошибке вместо результата деления.   
+
+try
+{
+    Console.Write("Vvedite pervoe chislo : ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Vvedite vtoroe chislo : ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    int result = num1 / num2;
+    Console.WriteLine(result);
+}
+catch (Exception e)
+{
+    Console.WriteLine($" One of the num is not good : {e.Message}");
+}
+finally
+{
+    Console.WriteLine($"The 'try catch' is finished. ");
+}
---
+++ 3_02_2026_lecture20/ClassWork/Task_4/Program.cs
@@ -0,0 +1,27 @@
+﻿
+//Напишите программу, которая запрашивает у пользователя номер месяца (от 1 до 12) и выводит название месяца с помощью перечисления (enum).
+
+Console.Write("Input number : ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if( n >= 1 && n <= 12)
+{
+    Month month = (Month)n;
+    Console.WriteLine(month);
+}
+
+enum Month
+{
+    January = 1,
+    February ,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    October,
+    November,
+    December
+}
